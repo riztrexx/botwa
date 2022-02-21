5a23b6bc13cf4d7ebec0ad981705f455edf660da
Riztrexx <67250292+riztrexx@users.noreply.github.com>
2022-02-21 15:27:07 +0800

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,15 @@
+//Jangan Di Hapus Tapi kalo Di tambah Boleh
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+┌〔 Donasi • Emoney 〕
+├ Ovo: 087720673203 [Fell]
+├ Dana: 087720673203 [Leaf]
+├ Pulsa: 0877206732035 [Fail]
+└────
+`.trim(), watermark, 'Menu', '.menu', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
